04c494e484b7d53f5dd10a29759a6a8687e0723d
Aaryan Sayzz <134924386+Aaryan2520@users.noreply.github.com>
2024-08-22 18:48:17 +0530

    Create index.js

---
+++ Video 58/index.js
@@ -0,0 +1,16 @@
+function nice(name) {
+    console.log("Hey " + name + " you are nice!")
+    console.log("Hey " + name + " you are good!")
+    console.log("Hey " + name + " your tshirt is nice!")
+    console.log("Hey " + name + " your course is good too!")
+}
+nice("Harry")
+
+function sum(a, b) {
+    // console.log(a + b)
+    return a + b
+}
+sum(1, 2)
+
+result = sum(12 , 88)
+console.log("The sum of there numbers is : ", result)
