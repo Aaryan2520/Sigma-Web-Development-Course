918065296e279432af11a40cae68e06b0a4147d1
Aaryan Sayzz <134924386+Aaryan2520@users.noreply.github.com>
2024-08-24 22:24:08 +0530

    Update script.js

--- Video 55/script.js
+++ Video 55/script.js
@@ -27,7 +27,7 @@ const p = true;
 let q = undefined;
 let r = null;
 
-console.log(x,"  ", y,"  ", z,"  ", p,"", q," ", r)
+console.log(x,"   ", y,"  ", z,"  ", p,"", q," ", r)
 console.log("",typeof x,"    ", typeof y, typeof z, typeof p, typeof q,"", typeof r)
 
 
